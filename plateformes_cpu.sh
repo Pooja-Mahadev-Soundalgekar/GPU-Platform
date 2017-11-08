6fd1827ede34e1e5399958ba80702ecbd1fb26b4
Pooja Soundalgekar <pooja27ms@gmail.com>
2017-11-08 10:32:35 +0530

    cpu

---
+++ plateformes_cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+#SBATCH --job-name=test_OpenCL_cpu
+#SBATCH --partition=debug
+#SBATCH --time=15:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+
+
+srun /home/albuquer/plateformes/plateformes_OpenCL
